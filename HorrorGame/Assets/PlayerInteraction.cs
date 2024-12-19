b641d724ecdc64dbb148df6e7e69c30f855b9a98
lilmonix3UwU <Tbnielsen2005@gmail.com>
2024-12-19 10:34:17 +0100

    moar doaar

---
+++ HorrorGame/Assets/PlayerInteraction.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    private GameObject PlayerCameraRoot;
+    private RaycastHit hit;
+    [SerializeField] private float InteractionRange;
+    void Start()
+    {
+        PlayerCameraRoot = GameObject.FindWithTag("CinemachineTarget"); 
+    }
+
+
+    void Update()
+    {
+        
+        Debug.DrawRay(PlayerCameraRoot.transform.position, PlayerCameraRoot.transform.forward, Color.red);
+        if (Physics.Raycast(PlayerCameraRoot.transform.position, PlayerCameraRoot.transform.forward, out hit, InteractionRange))
+        {
+            if (hit.collider.gameObject.CompareTag("DoorEnter"))
+            {
+                Debug.Log("pp");
+            }
+        }
+
+    }
+}
