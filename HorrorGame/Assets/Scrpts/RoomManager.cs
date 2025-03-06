5b85d1ddf89904dd9cb68d8b99d4d06b637cd6de
lilmonix3UwU <Tbnielsen2005@gmail.com>
2025-03-06 09:13:49 +0100

    movering af scripts

---
+++ HorrorGame/Assets/Scrpts/RoomManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    private GameObject controllers;
+    private GameObject anomilyController;
+    private GameObject pointsAndGameOver;
+
+    //public void 
+}
